dcf893505fa22f3fa2104062f2085adcbfa05b9d
Prince Charles <princecharles35@yahoo.com>
2019-10-26 19:46:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,27 @@ class Song
     self.class.all << self
   end
 
-end
+  def self.create
+    song = self.new
+    #@@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
